9629ac2d12701533498476eb1313267e3601fbcc
SanderSita <sandersekreve@gmail.com>
2024-08-24 00:05:36 +0200

    feat: get coin data

--- crypto-realtime-prices/api.py
+++ crypto-realtime-prices/api.py
@@ -1,14 +1,22 @@
 import httpx
+from urls import urls
 
 class API:
-    def __init__():
+    def __init__(self):
         pass
     
     def get_coin_data(self, slug: str):
-        pass
+        res = httpx.get(urls["token_data"] + slug)
+        json_data = res.json()
+        return json_data
     
     def get_current_price(self, slug: str):
         pass
     
     def get_realtime_price(self, slug: str):
         pass
+    
+if __name__ == "__main__":
+    api = API()
+    coin_data = api.get_coin_data("bitcoin")
+    print(coin_data)
--- crypto-realtime-prices/urls.py
+++ crypto-realtime-prices/urls.py
@@ -1,3 +1,6 @@
-urls = [
-    "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/detail/?slug=vechain",
-]
+# API URL EXAMPLES
+# https://api.coinmarketcap.com/data-api/v3/cryptocurrency/detail/?slug=vechain
+
+urls = {
+    "token_data": "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/detail/?slug="
+}
