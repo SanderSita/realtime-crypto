30ebb147322c46d749228102678242a3f4f368c3
Sander Sekreve <=>
2024-09-14 17:41:46 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ setuptools.setup(
     version="0.0.1",
     author="SanderSita",
     author_email="sandersekreve@gmail.com",
-    description="Python package for getting cryptocurrency prices and data from coinmarketcap",
+    description="A simple CoinmarketCap API for getting cryptocurrency prices and data",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/SanderSita/realtime-crypto",
+    project_urls={"Source": "https://github.com/SanderSita/realtime-crypto"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
